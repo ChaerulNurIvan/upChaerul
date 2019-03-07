fa212d95613f38f3cfb6cf2185e654b0576cc5bd
ChaerulNurIvan <48321937+ChaerulNurIvan@users.noreply.github.com>
2019-03-07 21:51:33 +0700

    Add files via upload

---
+++ Chaerul.java
@@ -0,0 +1,13 @@
+package Chaerul;
+
+/**
+ *
+ * @author vans
+ */
+public class Chaerul {
+    public static void main(String[] args) {
+        System.out.println("Nama  : ChaerulNurIvan\n");
+        System.out.println("NIM   : 17090056\n");
+        System.out.println("Kelas : TI 4A");
+    }  
+}
